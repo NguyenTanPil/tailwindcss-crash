2852eab4773fa51ae84e341f47bf0491f18accf2
NguyenTanPil <traila1008@gmail.com>
2024-04-15 22:52:47 +0700

    Getting started

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 export default {
 	plugins: {
 		'postcss-import': {},
+		'tailwindcss/nesting': 'postcss-nesting',
 		tailwindcss: {},
 		autoprefixer: {},
+		'postcss-preset-env': {
+			features: { 'nesting-rules': false },
+		},
+		cssnano: {},
 	},
 };
