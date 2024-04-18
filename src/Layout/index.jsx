e886aca57f6e07674a54d724d8450f56055a992a
NguyenTanPil <traila1008@gmail.com>
2024-04-18 23:33:33 +0700

    Completed basic

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -2,7 +2,7 @@
 // import Column from './Column';
 // import Container from './Container';
 // import Break from './Break';
-import ObjectPosition from './ObjectPosition';
+// import ObjectPosition from './ObjectPosition';
 
 const Layout = () => {
 	return (
@@ -13,7 +13,13 @@ const Layout = () => {
 			{/* <Container /> */}
 			{/* <Column /> */}
 			{/* <Break /> */}
-			<ObjectPosition />
+			{/* <ObjectPosition /> */}
+			<div className='grid grid-cols-[1fr_500px_2fr]'></div>
+			<button className='md:[&:nth-child(1)]:bg-white py-2 px-5 bg-violet-500 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75'>
+				Save changes 1
+			</button>
+
+			<button className='btn-primary'>Save changes 2</button>
 		</div>
 	);
 };
